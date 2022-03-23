8cbb7afe83f131b72a52761d443169d0cc9ed496
cinuor <cokie@foxmail.com>
2022-03-23 23:50:14 +0800

    fix: correct internal test function name

--- lua/gotests/init.lua
+++ lua/gotests/init.lua
@@ -47,7 +47,11 @@ local extra_info = function(gocwd, gofile, funame)
 
 	local test_func = nil
 	if type(funame) == "string" and #funame ~= 0 then
-		test_func = "Test" .. funame
+		if string.match(funame[1], "%u") then
+			test_func = "Test" .. funame
+		else
+			test_func = "Test_" .. funame
+		end
 	end
 
 	extra.test_gocwd = gocwd
